4312cafb1bd40d4e604e00c394ba5a37e562670c
pricen3 <nolan.price@gmail.com>
2025-02-10 13:05:33 -0800

    updating for DataPipeline Package 0.6.0

--- code/Pipeline/src/pipeline.py
+++ code/Pipeline/src/pipeline.py
@@ -1,19 +1,25 @@
 from extract import extract
 from transform import transform
 from load import load
+import traceback
 from ecopipeline import ConfigManager
 
 config = ConfigManager()
-merged_data, last_full_day = extract(config)
-print("last full day in database found", last_full_day)
+try:
+    merged_data, last_full_day = extract(config)
+    print("last full day in database found", last_full_day)
 
-transformed_data, hourly_data, daily_data = transform(merged_data.copy(), config)
+    transformed_data, hourly_data, daily_data = transform(merged_data.copy(), config)
 
-dfs_to_load = {"minute": transformed_data,
-               "hour": hourly_data,
-               "day": daily_data}
+    dfs_to_load = {"minute": transformed_data,
+                "hour": hourly_data,
+                "day": daily_data}
 
-load(dfs_to_load, config)
+    load(dfs_to_load, config)
+except Exception as e: 
+        traceback.print_exc()
+        # uncomment below to report data loss
+        # report_data_loss(config)
 
 
 
