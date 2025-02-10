97078c407d83cb7b04ead0723952bab38967d0f6
pricen3 <nolan.price@gmail.com>
2025-02-10 13:13:30 -0800

    updating for DataPipeline Package 0.6.0

--- code/Pipeline/src/load.py
+++ code/Pipeline/src/load.py
@@ -1,22 +1,12 @@
 from ecopipeline.load import load_overwrite_database
 from ecopipeline import ConfigManager
-import pandas as pd
-import traceback
+
 
 def load(load_dfs: dict, config : ConfigManager):
     
-    config_dict = config.get_db_table_info(load_dfs.keys()) 
-    db_connection, db_cursor = config.connect_db()
+    config_dict = config.get_db_table_info(load_dfs.keys())
     
-    try:
-    
-        for df in load_dfs:
-            
-            load_overwrite_database(cursor = db_cursor, dataframe = load_dfs[df], config_info = config_dict, data_type = df)
-            db_connection.commit()
-    except Exception as e: traceback.print_exc()
-        
-        
-    db_connection.close()
-    db_cursor.close()
+    for df in load_dfs:   
+        load_overwrite_database(config, dataframe = load_dfs[df], config_info = config_dict, data_type = df)
+
     
--- code/Pipeline/src/pipeline.py
+++ code/Pipeline/src/pipeline.py
@@ -2,19 +2,25 @@ from extract import extract
 from transform import transform
 from load import load
 from ecopipeline import ConfigManager
+from ecopipeline.load import report_data_loss
+import traceback
 
 config = ConfigManager()
-merged_data, last_full_day = extract(config)
-print("last full day in database found", last_full_day)
+try:
+    merged_data, last_full_day = extract(config)
+    print("last full day in database found", last_full_day)
 
-transformed_data, hourly_data, daily_data = transform(merged_data.copy(), config)
+    transformed_data, hourly_data, daily_data = transform(merged_data.copy(), config)
 
-dfs_to_load = {"minute": transformed_data,
-               "hour": hourly_data,
-               "day": daily_data}
+    dfs_to_load = {"minute": transformed_data,
+                "hour": hourly_data,
+                "day": daily_data}
 
-load(dfs_to_load, config)
+    load(dfs_to_load, config)
 
+except Exception as e: 
+    traceback.print_exc()
+    report_data_loss()
 
 
 
